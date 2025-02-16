d13692bfb5df642b5a6c3c09fa26dd00625f541c
Wassim Assidi <wassim.assidi@astrolab-group.com>
2025-02-16 15:35:09 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 				>
-					<Link to={"/"}>Le bon développeur 🇹🇳 - Store 🛒</Link>
+					<Link to={"/"}>Le bon développeur - Store 🛒</Link>
 				</Text>
 
 				<HStack spacing={2} alignItems={"center"}>
